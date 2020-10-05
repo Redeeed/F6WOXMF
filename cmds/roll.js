ccc64070816a320201ff6a214dc2f958530671ac
Redeeed <72359402+Redeeed@users.noreply.github.com>
2020-10-05 21:42:44 +0300

    Roll fixed 3

--- cmds/roll.js
+++ cmds/roll.js
@@ -3,13 +3,14 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     if(!args[0]) return bot.send("Вы не указали число");
     let args2 = (!args)
-    if(args2 < 0) return bot.send("Кубик полетел в 4 измерение(выпало отрицательное число")
+    
 
     
     
      
     let number = args.join(" ");
     let answer = (Math.floor(Math.random() * number+1))
+    if (number < 1) return bot.send("Кубик улетел в 4 измерение(выпало отрицательное число)")
     let embed = new Discord.RichEmbed()
     
     .setTitle(`Вам выпало число:`)
