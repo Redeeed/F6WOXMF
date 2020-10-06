6a91f3217252f3c398b54dd0b6d2be148f14bc2b
Redeeed <72359402+Redeeed@users.noreply.github.com>
2020-10-06 20:55:07 +0300

    Profilefix added

--- cmds/profilefix.js
+++ cmds/profilefix.js
@@ -8,11 +8,11 @@ module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.find(m => m.user.username == args[0] || m.id == args[0])) // участник
     if(!args[0]) return bot.send("Вы не указали пользователя");
     
-    if(!profile[rUser.id])return bot.send("Пользователя нету в profile.json");
-    
-    fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
+    if(!profile[rUser.id]) return fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
         if(err) console.log(err);
     });
+    
+    
     if(profile[rUser.id].warns >=5){
         message.guild.member(rUser).kick("5/5 Предупреждений");
     }
