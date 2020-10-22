9fa2c139805cc85d7a386af6de847015905899b9
Redeeed <72359402+Redeeed@users.noreply.github.com>
2020-10-22 19:12:32 +0300

    kekeke

--- cmds/dm.js
+++ cmds/dm.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message,args) => {
     if(message.author.tag !== 'Fairon#1658') return message.channel.send("У вас нет прав");
      
     let botmessage = args.slice(2).join(' ');
-    let rUser = message.guild.member(m.name == args[1]) // участник
+    let rUser = message.guild.member( message.guild.members.find(m => m.user.username == args[0] || m.id == args[0])) // участник
     message.delete().catch();
     rUser.send(botmessage);
 };
