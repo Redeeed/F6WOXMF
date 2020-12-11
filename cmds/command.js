e01ea14dc50693c8efb63ae1e30f05d6c670562f
Redeeed <72359402+Redeeed@users.noreply.github.com>
2020-12-11 16:04:48 +0300

    F

--- cmds/command.js
+++ cmds/command.js
@@ -2,9 +2,12 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     if(message.author.tag !== 'Fairon#1658') return message.channel.send("У вас нет прав");
-     
-    let botmessage = args.join(" ");
-    let channelid = bot.channels.get("756211020342755451");
+    
+    
+    let channel = args.slice(1).join(' ');
+    let botmessage = args.slice(2).join(' ');
+    
+    let channelid = bot.channels.get(`${channel}`);
     message.delete().catch();
     channelid.send(botmessage);
 };
--- cmds/help.js
+++ cmds/help.js
@@ -4,17 +4,17 @@ module.exports.run = async (bot,message,args) => {
    
     let embed = new Discord.RichEmbed()
     .setTitle('Использование команд:')
-    .addField("!ban","!ban Участник Причина")
-    .addField("!clear","!clear 1-100")
-    .addField("!kick","!kick Участник Причина")
-    .addField("!mute","!mute Участник Время(в секундах) Причина")
+    .addField("(выключено)!ban","!ban Участник Причина")
+    .addField("(выключено)!clear","!clear 1-100")
+    .addField("(выключено)!kick","!kick Участник Причина")
+    .addField("(выключено)!mute","!mute Участник Время(в секундах) Причина")
     .addField("!roll-генератор случайных чисел","!roll максимальное число(при больших значениях результаты могут стать странными)")
     .addField("!serverinfo","Информация о сервере")
-    .addField("!profile-отображает профиль пользователя","!profile (будет введена возможность просмотра профиля других людей)")
-    .addField("!unmute","!unmute Участник")
-    .addField("!unwarn","!unwarn Участник ")
+    .addField("(выключено)!profile-отображает профиль пользователя","!profile (будет введена возможность просмотра профиля других людей)")
+    .addField("!(выключено)unmute","!unmute Участник")
+    .addField("(выключено)!unwarn","!unwarn Участник ")
     .addField("!userinfo","Информация о участнике")
-    .addField("!warn","!warn Участник Причина")
+    .addField("(выключено)!warn","!warn Участник Причина")
     
     message.channel.send(embed);
 };
